b9af1c4b232ec7e2c252ecc60136977ebaf67746
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-08-20 21:18:32 +0800

    not gonna type document.getElement as often

---
+++ js/basic.js
@@ -0,0 +1,8 @@
+/**
+ * Shortens the document.getElementById stuff
+ * @param {string} id id of the element
+ * @returns {HTMLElement}
+ */
+export function getDoc(id) {
+    return document.getElementById(id)
+};
