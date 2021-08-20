82b0d2e5099213d2e09f7b7b994b762771fea6db
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-08-20 20:13:27 +0800

    restarting

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,30 @@
 <script>
-    let count = 0;
-
-    function sum() {
-        count++;
-    }
+	export let name;
 </script>
 
-<p>Counter: {count}</p>
-<button on:click={sum}> + </button>
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
 
 <style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
