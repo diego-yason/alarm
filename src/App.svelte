dd55cbdfa0793e9ea4bc0106cd78e51b5c4db755
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-08-22 03:03:32 +0800

    added alarm and scss

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
+<script>
+    import Alarm from "./components/Alarm.svelte";
+</script>
+
 <body>
     <h1 id="title">Alarms</h1>
+    <Alarm/>
 </body>
 
-<style>
+<style lang="scss">
     body {
         min-width: 250px;
         min-height: 400px;
+
+        h1 {
+            text-align: center;
+        }
     }
 </style>
